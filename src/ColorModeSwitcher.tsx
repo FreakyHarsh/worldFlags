00d14b95ce81cac14f54d343f827164a1f4afa98
FreakyHarsh <harsh.boricha2015@gmail.com>
2021-01-07 13:17:48 +0530

    style: light and dark mode

--- src/ColorModeSwitcher.tsx
+++ src/ColorModeSwitcher.tsx
@@ -1,30 +1,38 @@
-import * as React from "react"
+import * as React from 'react';
 import {
   useColorMode,
   useColorModeValue,
   IconButton,
   IconButtonProps,
-} from "@chakra-ui/react"
-import { FaMoon, FaSun } from "react-icons/fa"
+  Text,
+  Flex,
+} from '@chakra-ui/react';
+import { FaMoon, FaSun } from 'react-icons/fa';
 
-type ColorModeSwitcherProps = Omit<IconButtonProps, "aria-label">
+type ColorModeSwitcherProps = Omit<IconButtonProps, 'aria-label'>;
 
 export const ColorModeSwitcher: React.FC<ColorModeSwitcherProps> = (props) => {
-  const { toggleColorMode } = useColorMode()
-  const text = useColorModeValue("dark", "light")
-  const SwitchIcon = useColorModeValue(FaMoon, FaSun)
-
+  const { colorMode, toggleColorMode } = useColorMode();
+  const SwitchIcon = useColorModeValue(FaMoon, FaSun);
+  const colorModeText = colorMode === 'light' ? 'Light Mode' : 'Dark Mode';
   return (
     <IconButton
-      size="md"
-      fontSize="lg"
-      variant="ghost"
-      color="current"
-      marginLeft="2"
+      size='md'
+      fontSize={['sm', 'md', 'lg', 'xl']}
+      variant='ghost'
+      color='current'
+      marginLeft='2'
       onClick={toggleColorMode}
-      icon={<SwitchIcon />}
-      aria-label={`Switch to ${text} mode`}
+      icon={
+        <Flex p={2}>
+          <SwitchIcon />
+          <Text fontSize={['sm', 'md', 'lg', 'xl']} fontWeight={400} ml={2}>
+            {colorModeText}
+          </Text>
+        </Flex>
+      }
+      aria-label={`Switch to mode`}
       {...props}
     />
-  )
-}
+  );
+};
