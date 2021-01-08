c3d9d83d20df588c941fd59cc1111fe6eb91310e
FreakyHarsh <harsh.boricha2015@gmail.com>
2021-01-08 22:17:53 +0530

    fix: dark mode btn

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Flex, Divider, Heading, Link } from '@chakra-ui/react';
+import { Flex, Divider, Heading, Link, Box } from '@chakra-ui/react';
 import { Link as ReactRouterLink } from 'react-router-dom';
 import { ColorModeSwitcher } from '../ColorModeSwitcher';
 import { useStore } from '../store/store';
@@ -16,16 +16,10 @@ export const Navbar: React.FC = () => {
   };
   return (
     <>
-      <Flex
-        align='center'
-        as={ReactRouterLink}
-        to='/'
-        onClick={makeDefaultHandler}
-        justify='space-between'
-        p={2}
-        mx='5%'
-      >
-        <Heading fontSize={[20, 'md', 'lg', 'xl']}>Where in the world?</Heading>
+      <Flex align='center' justify='space-between' p={2} mx='5%'>
+        <Box as={ReactRouterLink} to='/' onClick={makeDefaultHandler}>
+          <Heading fontSize={[20, 'md', 'lg', 'xl']}>Where in the world?</Heading>
+        </Box>
         <ColorModeSwitcher />
       </Flex>
       <Divider />
